f375f16685d38ad9acfdd6383055c29f8c9b4a81
Brendan Murty <b@murty.io>
2019-12-03 10:25:12 +1100

    Core: Update Fathom analytics embed code

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -41,18 +41,20 @@
             </footer>
         </section>
 
+        <!-- Fathom - simple website analytics - https://usefathom.com -->
         <script>
         (function(f, a, t, h, o, m){
-        	a[h]=a[h]||function(){
-        		(a[h].q=a[h].q||[]).push(arguments)
-        	};
-        	o=f.createElement('script'),
-        	m=f.getElementsByTagName('script')[0];
-        	o.async=1; o.src=t; o.id='fathom-script';
-        	m.parentNode.insertBefore(o,m)
-        })(document, window, '//stats.murty.io/tracker.js', 'fathom');
-        fathom('set', 'siteId', 'VJUUO');
+        a[h]=a[h]||function(){
+        (a[h].q=a[h].q||[]).push(arguments)
+        };
+        o=f.createElement('script'),
+        m=f.getElementsByTagName('script')[0];
+        o.async=1; o.src=t; o.id='fathom-script';
+        m.parentNode.insertBefore(o,m)
+        })(document, window, 'https://cdn.usefathom.com/tracker.js', 'fathom');
+        fathom('set', 'siteId', 'KUJKWGJX');
         fathom('trackPageview');
         </script>
+        <!-- / Fathom -->
     </body>
 </html>
