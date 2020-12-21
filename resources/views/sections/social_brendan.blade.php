7f08087a46f2deccb983c144894adc16959622fb
Brendan Murty <b@murty.io>
2020-12-21 19:17:51 +1100

    Brendan: Re-order items in social links section of index page

--- resources/views/sections/social_brendan.blade.php
+++ resources/views/sections/social_brendan.blade.php
@@ -1,14 +1,5 @@
 <ul class="listing social">
     <li>
-        <a href="mailto:b@murty.io" title="Send me an email at b@murty.io">
-            <svg xmlns="http://www.w3.org/2000/svg" role="img" width="40" height="40" viewBox="0 0 24 24" fill="none" stroke="#fff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"/>
-                <polyline points="22,6 12,13 2,6"/>
-            </svg>
-            <span>Email</span>
-        </a>
-    </li>
-    <li>
         <a href="/brendan/posts" title="View my Posts">
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#fff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-file-text">
                 <path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path>
@@ -30,6 +21,17 @@
         </a>
     </li>
     <li>
+        <a href="https://github.com/brendanmurty?tab=repositories" title="View my code repositories on GitHub">
+            <svg xmlns="http://www.w3.org/2000/svg" role="img" width="40" height="40" viewBox="0 0 24 24" fill="none" stroke="#fff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-git-branch">
+                <line x1="6" y1="3" x2="6" y2="15"></line>
+                <circle cx="18" cy="6" r="3"></circle>
+                <circle cx="6" cy="18" r="3"></circle>
+                <path d="M18 9a9 9 0 0 1-9 9"></path>
+            </svg>
+            <span>Git</span>
+        </a>
+    </li>
+    <li>
         <a href="https://www.linkedin.com/in/brendanmurty/" title="View my LinkedIn profile">
             <svg xmlns="http://www.w3.org/2000/svg" role="img" width="40" height="40" viewBox="0 0 24 24" fill="none" stroke="#fff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-linkedin">
                 <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"></path>
@@ -40,14 +42,12 @@
         </a>
     </li>
     <li>
-        <a href="https://github.com/brendanmurty?tab=repositories" title="View my code repositories on GitHub">
-            <svg xmlns="http://www.w3.org/2000/svg" role="img" width="40" height="40" viewBox="0 0 24 24" fill="none" stroke="#fff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-git-branch">
-                <line x1="6" y1="3" x2="6" y2="15"></line>
-                <circle cx="18" cy="6" r="3"></circle>
-                <circle cx="6" cy="18" r="3"></circle>
-                <path d="M18 9a9 9 0 0 1-9 9"></path>
+        <a href="mailto:b@murty.io" title="Send me an email at b@murty.io">
+            <svg xmlns="http://www.w3.org/2000/svg" role="img" width="40" height="40" viewBox="0 0 24 24" fill="none" stroke="#fff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"/>
+                <polyline points="22,6 12,13 2,6"/>
             </svg>
-            <span>Git</span>
+            <span>Email</span>
         </a>
     </li>
     <li>
